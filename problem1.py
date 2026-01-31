4c43628958a12c874b9c051c595e05ac136bc5cb
Amanpratap22 <amanpratap764@gmail.com>
2026-01-31 20:15:09 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,9 @@
+# Create a dictionary to store personal information
+my_info = {
+    "name": "Aman",
+    "age": 20,
+    "gender": "Male"
+}
+
+print("My Information:")
+print(my_info)
---
+++ problem2.py
@@ -0,0 +1,11 @@
+# Create a dictionary
+student = {
+    "name": "Aman",
+    "age": 20,
+    "gender": "Male"
+}
+
+# Access items using key names
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Gender:", student["gender"])
