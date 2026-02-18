1616be2f466731b1da93fff6e1d076fdcef4d03c
Amanpratap22 <amanpratap764@gmail.com>
2026-02-18 10:48:12 +0530

    Add files via upload

---
+++ problem9.py
@@ -0,0 +1,16 @@
+# Create two dictionaries
+dict1 = {
+    "name": "Aman",
+    "age": 21
+}
+
+dict2 = {
+    "course": "B.Tech",
+    "city": "Noida"
+}
+
+# Merge the dictionaries
+merged_dict = dict1 | dict2   # Python 3.9+
+
+# Print the merged dictionary
+print(merged_dict)
