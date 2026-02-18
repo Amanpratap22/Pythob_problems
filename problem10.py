fd8f02ed144dfaea350d074c70dcc195037c4bcb
Amanpratap22 <amanpratap764@gmail.com>
2026-02-18 10:49:14 +0530

    Add files via upload

---
+++ problem10.py
@@ -0,0 +1,37 @@
+# Given dictionary
+sample_dict = {'C': 92, 'java': 66, 'python': 85}
+
+# Get the key with the lowest value
+lowest_key = min(sample_dict, key=sample_dict.get)
+
+# Print the result
+print("Key with lowest value:", lowest_key)
+
+
+
+
+# Create a dictionary to store personal information
+my_info = {
+    "name": "Aman",
+    "age": 20,
+    "gender": "Male"
+}
+
+print("My Information:")
+print(my_info)
+
+
+
+# Create a dictionary
+student = {
+    "name": "Aman",
+    "age": 20,
+    "gender": "Male"
+}
+
+# Access items using key names
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Gender:", student["gender"])
+
+
